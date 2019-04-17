0d7937392eb6c7eaa37009e16fd9747506536b76
Victor Clouet d'Orval <victorclouetdorval@orange.fr>
2019-04-18 00:55:41 +0200

    actualisation code electron esp32

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,71 @@
 char ssid[] = "INTech2";
 char pass[] = "$0P@L1Z7";
 int status = WL_IDLE_STATUS;
-WiFiServer server(1296);
+WiFiServer server;
 WiFiClient client;
-int moteurs = 22;
-int contacteur = 23;
+int moteurs = 13;
+int contacteur = 34;
+int led = 35;
 String stopmessage="INTech Tech The Respect\n";
 String runmessage="Que la force soit avec toi\n";
 String arretedecrire="A";
 bool test = true;
+String message;
+bool moteuractive = false;
+bool arrive = false;
+
+void setup()
+{
+    delay(5000);
+    Serial.begin(9600);
+    pinMode(moteurs, OUTPUT);
+    digitalWrite(moteurs, LOW);
+    pinMode(contacteur, INPUT_PULLUP);
+    pinMode(led,OUTPUT);
+    digitalWrite(led,HIGH);
+
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(ssid,pass);
+    delay(100);
+    WiFi.softAPConfig(IPAddress(192,168,5,1),IPAddress(192,168,5,1),IPAddress(255,255,255,0));
+
+    server.begin(18900);
+
+}
+
+void loop()
+{
+    while (!client) {
+        client = server.available();
+        delay(10);
+        if (digitalRead(contacteur) == LOW)
+        {
+            digitalWrite(moteurs,LOW);
+            arrive = true;
+        }
+
+    }
+    message = client.readString();
+    if (message == "electron_launch"){
+        if (!moteuractive) {
+            digitalWrite(moteurs, HIGH);
+            moteuractive = true;
+        }
+        client.print("@Belectron_activated\n");
 
 
+    }
+    if (digitalRead(contacteur) == LOW)
+    {
+        digitalWrite(moteurs,LOW);
+        arrive = true;
+    }
+    if (arrive) {
+        client.print("@Belectron_arrived\n");
+    }
+}
+
+/*
 void setup() {
     delay(5000);
     Serial.begin(9600);
@@ -66,4 +121,4 @@ void loop() {
     }
 
 }
-
+ */
