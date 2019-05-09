3712a3679fc9c6e669f78366a149cc13576a85c4
jglrxavpok <jglrxavpok@gmail.com>
2019-05-09 17:29:36 +0200

    Nettoyage + utilisation de la bonne pin. Electron qui marche!

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,17 @@
 
 /////////////////CONFIG WIFI///////////////////////
 const char ssid[] = "nucsamere";
-//const char pass[] = "$0P@L1Z7";
 const char pass[]= "suussuus";
-//const byte serverAdress[] = {192, 168, 12, 247};
 const int serverPort=18900;
 WiFiClient client;
 WiFiServer server(serverPort);
 
 
 ///////////////////GPIO///////////////////////////
-int moteurs = 13;
-int contacteur = 32;
-int led = 33;
+uint8_t moteurs = 12;
+uint8_t contacteur = 32;
+uint8_t led = 33;
+uint8_t BLUE_LED = 2;
 const int PWMfreq = 1000;
 const int PWMChannel = 1;
 const int PWMresolution = 8;
@@ -51,11 +50,7 @@ void setup()
     attachInterrupt(digitalPinToInterrupt(contacteur), arriver, FALLING);
     ledcWrite(PWMChannel,100);
     Serial.println("init Wifi");
-    /*
-    WiFi.mode(WIFI_AP);
-    WiFi.softAP(ssid,pass);
-    WiFi.softAPConfig(IPAddress(192,168,5,1),IPAddress(192,168,5,1),IPAddress(255,255,255,0));
-    */
+
     WiFi.begin(ssid, pass);
     server.begin(serverPort);
     while (WiFi.status() != WL_CONNECTED) {
@@ -72,35 +67,37 @@ void loop()
 {
     ledcWrite(PWMChannel, 255);
     //Serial.println(WiFi.status());
+    digitalWrite(BLUE_LED, LOW);
+    bool hadClient = client;
     while ( ! client) {
-        //Serial.println("attente client");
         client = server.available();
         delay(100);
     }
-    Serial.println("Connecté à "+client.remoteIP().toString());
-    delay(100);
+    if(!hadClient) {
+        Serial.println("Connecté à "+client.remoteIP().toString());
+    }
+    digitalWrite(BLUE_LED, HIGH);
     Serial.println(client.available());
-    if(client.available()>0) {
+    if(client.available() > 0) {
         String data=client.readStringUntil('\n');
         if (data.equals("electron_launch")) {
             Serial.println("electron activated");
-            ledcWrite(PWMChannel, 255);
-            delay(200);
-            ledcWrite(PWMChannel, 100);
-            if (!moteuractive) {
-                digitalWrite(moteurs, HIGH);
-                moteuractive = true;
-            }
             client.print("@Belectron_activated\n");
-        }else{
+        } else {
             Serial.println("Recu message inattendu: "+data);
         }
-    } else{
-        Serial.println("Rien recu");
+    }
+
+    if (!moteuractive) {
+        digitalWrite(moteurs, HIGH);
+        moteuractive = true;
     }
 
     if (arrive) {
+        ledcWrite(PWMChannel, 100);
         client.print("@Belectron_arrived\n");
-
+    } else {
+        ledcWrite(PWMChannel, 255);
     }
+    delay(100);
 }
